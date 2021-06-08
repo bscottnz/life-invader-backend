952c12c3cc3f773233c2fd4267d8e9f64e6a633c
Ben <blscott@gmail.com>
2021-06-08 15:59:15 +1200

    add new message to db

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -5,6 +5,7 @@ const { body } = require('express-validator');
 const User = require('../../models/UserSchema');
 const Post = require('../../models/PostSchema');
 const Chat = require('../../models/ChatSchema');
+const Message = require('../../models/MessageSchema');
 
 router.get('/', async (req, res, next) => {
   res.send('Inbox page');
@@ -12,6 +13,28 @@ router.get('/', async (req, res, next) => {
 
 router.get('/new', async (req, res, next) => {
   res.send('New Message');
+});
+
+router.post('/', (req, res, next) => {
+  if (!req.body.content || !req.body.chatId) {
+    console.log('content or id not sent with create message request');
+    return res.sendStatus(400);
+  }
+
+  const newMessage = {
+    sender: req.user._id,
+    content: req.body.content,
+    chat: req.body.chatId,
+  };
+
+  Message.create(newMessage)
+    .then((results) => {
+      res.status(201).send(results);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
 });
 
 module.exports = router;
