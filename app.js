1f5b3545060099a828904bbc3e3f101a8c05440a
Ben <blscott@gmail.com>
2021-06-16 20:36:36 +1200

    cross origin middleware heroku

--- app.js
+++ app.js
@@ -62,6 +62,17 @@ app.use(logger('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  );
+  next();
+});
+
 // routes -----------------------------------------------------------------
 
 app.use('/login', loginRoute);
